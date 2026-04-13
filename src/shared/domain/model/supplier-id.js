b530611f4bf60a0109f66dd551eb07b26180c18b
Leonardo Cabrera <u202415820@upc.edu.pe>
2026-04-13 11:47:09 -0500

    feat(supplier-id): add supplier-id value object.

---
+++ src/shared/domain/model/supplier-id.js
@@ -0,0 +1,24 @@
+import {generateUuid, validateUuid} from "./uuid.js";
+import {ValidationError} from "./errors.js";
+
+export class SupplierId {
+    #value;
+
+    constructor(value) {
+        if (!validateUuid(value))
+            throw new ValidationError('Invalid Supplier Id: ${value}. Must be a valid UUID.')
+        this.#value = value;
+    }
+
+    static generate(){
+        return new SupplierId(generateUuid());
+    }
+
+    get value(){
+        return this.#value;
+    }
+
+    equals(other){
+        return other instanceof SupplierId && this.#value === other.value;
+    }
+}
